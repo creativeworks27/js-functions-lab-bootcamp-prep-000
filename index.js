cd9bef359ab73bfc5fb3ce4867bcd8680f6cc6a5
Roberto Romero <creativeworks27@icloud.com>
2018-09-14 19:45:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidaysTo(Jhon) {
 function happyHolidayTo(Christmas, Jhon) {
   return(`Happy ${Christmas}, ${Jhon}!`);
 }
-function holidayCountdown(Christmas, 15) {
-  
+function holidayCountdown(Christmas,15) {
+  return(`It's ${15} days until ${Christmas}!`);
 }
